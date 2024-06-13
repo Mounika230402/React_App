f5033209339725f0699fee0ba243123013dfe3b0
Mounika230402 <kankanampatimounika@gmail.com>
2024-06-13 10:49:28 +0530

    file

--- src/components/Forms/form.js
+++ src/components/Forms/form.js
@@ -1,6 +1,6 @@
 export const FormComponent=()=>{
     return <form>
-        <label for="name">Name</label>
+        <label for="name">Name</label><br>
         <input type="text" id="name"></input>
         <label for="mail">Mail</label>
         <input type="email" id="mail"></input>
